18f55d15b4a6abef03c7ed85f47091fba9127670
Vlad Ostapenko <vladys.ostapenko@gmail.com>
2023-04-11 14:22:16 +0300

    Completed 4 tasks in HomeWork.py

---
+++ HomeWork_4.py
@@ -0,0 +1,44 @@
+# task 1: Валидатор почтового адреса
+email = "aaa@bbb.ccc"  # True
+if email.startswith('@') and email.endswith('.'):
+    print("False")
+else:
+    print('True')
+if email.count('@') and email.count('.') == 1:
+    print('True')
+else:
+    print('False')
+if email.find('@') < email.find('.'):
+    print('True')
+else:
+    print('False')
+
+
+# task 2: Поиск слов по условию
+s = "aab qq c badcc a qqqqqaqqqqaa tpara"
+words = s.split()
+result = []
+for word in words:
+    count_a = word.count('a')
+    if count_a == 2:
+        result.append(word)
+print(' '.join(result).title())
+
+
+# task 3: Количество слов в предложении
+multi_string = "Hello all. Here's pretty cold and hot. Choose yourself."
+sentences = multi_string.split('. ')
+words_number = []
+for sentence in sentences:
+    sentence = sentence.strip()
+    words = sentence.split(' ')
+    words = [word for word in words if word]
+    words_number.append(len(words))
+print(words_number)
+
+
+# task 4: Палиндром
+str1 = input().strip().lower()  # 1st input - "    aBC cba " , 2nd input - "a BCQcb a    " , 3rd input - " ab bca"
+str1_reversed = str1[::-1]
+print(str1 == str1_reversed)
+
